1ad54ea5557a5c68792ffeaf52e4a98c1997f666
David Keijser <keijser@gmail.com>
2014-11-27 15:12:40 +0100

    update inline documentation

--- matchmock.py
+++ matchmock.py
@@ -1,8 +1,19 @@
+'''Hamcrest matchers for mock objects'''
+
 from hamcrest.core.base_matcher import BaseMatcher
 from hamcrest import contains, equal_to, all_of, anything, has_entry, has_item
 
+__all__ = ['called', 'not_called', 'called_once',
+           'called_with', 'called_once_with']
+
 
 class Call(BaseMatcher):
+    '''A matcher that describes a call.
+
+    The positional arguments and keyword arguments are represented with
+    individual submatchers.
+    '''
+
     def __init__(self, args, kwargs):
         self.args = args
         self.kwargs = kwargs
@@ -24,14 +35,20 @@ class Call(BaseMatcher):
 
 
 def match_args(args):
+    '''Create a matcher for positional arguments'''
+
     return contains(*args)
 
 
 def match_kwargs(kwargs):
+    '''Create a matcher for keyword arguments'''
+
     return all_of(*[has_entry(k, v) for k, v in kwargs.items()])
 
 
 class Called(BaseMatcher):
+    '''Matches a mock and asserts the number of calls and parameters'''
+
     def __init__(self, call, count=None):
         self.call = call
         self.count = count
@@ -64,20 +81,30 @@ class Called(BaseMatcher):
 
 
 def called():
+    '''Match mock that was called one or more times'''
+
     return Called(anything())
 
 
 def not_called():
+    '''Match mock that was never called'''
+
     return Called(anything(), count=0)
 
 
 def called_once():
+    '''Match mock that was called once regardless of arguments'''
+
     return Called(anything(), count=1)
 
 
 def called_with(*args, **kwargs):
+    '''Match mock has at least one call with the specified arguments'''
+
     return Called(Call(match_args(args), match_kwargs(kwargs)))
 
 
 def called_once_with(*args, **kwargs):
+    '''Match mock that was called once and with the specified arguments'''
+
     return Called(Call(match_args(args), match_kwargs(kwargs)), count=1)
