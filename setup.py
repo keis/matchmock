6dc8a662bd37213efc06814f65fd497785ee86a2
David Keijser <keijser@gmail.com>
2017-01-09 10:56:07 +0100

    Version 0.7.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='matchmock',
-    version='0.7.0',
+    version='0.7.1',
     description='Hamcrest matchers for mock objects',
     url='https://github.com/keis/matchmock',
     py_modules=['matchmock']
