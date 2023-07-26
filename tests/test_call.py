0d0cdb2c6f1b9e277c1abf0c7207e91598acf835
David Keijser <keijser@gmail.com>
2023-07-26 11:22:32 +0200

    Add type annotation to test functions

--- tests/test_call.py
+++ tests/test_call.py
@@ -4,33 +4,33 @@ from matchmock import IsCall, match_args, match_kwargs
 from unittest.mock import call
 
 
-def test_matching_call():
+def test_matching_call() -> None:
     m = IsCall(match_args(('foo',)), match_kwargs({}))
     value = call('foo')
     assert_that(m.matches(value), equal_to(True))
 
 
-def test_mismatching_call():
+def test_mismatching_call() -> None:
     m = IsCall(match_args(('foo',)), match_kwargs({}))
     value = call('bar')
     assert_that(m.matches(value), equal_to(False))
 
 
-def test_describe_self():
+def test_describe_self() -> None:
     m = IsCall(match_args(('foo',)), match_kwargs({}))
     s = StringDescription()
     m.describe_to(s)
     assert_that(str(s), equal_to("('foo', )"))
 
 
-def test_describe_self_with_kwargs():
+def test_describe_self_with_kwargs() -> None:
     m = IsCall(match_args(('foo',)), match_kwargs({'key': 'value'}))
     s = StringDescription()
     m.describe_to(s)
     assert_that(str(s), equal_to("('foo', key='value')"))
 
 
-def test_describe_mismatch():
+def test_describe_mismatch() -> None:
     m = IsCall(match_args(('foo',)), match_kwargs({}))
     value = call('bar')
     s = StringDescription()
@@ -38,7 +38,7 @@ def test_describe_mismatch():
     assert_that(str(s), equal_to("argument 0: was 'bar'"))
 
 
-def test_describe_mismatch_kwargs():
+def test_describe_mismatch_kwargs() -> None:
     m = IsCall(match_args(('foo',)), match_kwargs({'key': 'value'}))
     value = call('foo', key='VALUE')
     s = StringDescription()
@@ -46,7 +46,7 @@ def test_describe_mismatch_kwargs():
     assert_that(str(s), equal_to("value for 'key' was 'VALUE'"))
 
 
-def test_args_mismatch_complex():
+def test_args_mismatch_complex() -> None:
     m = IsCall(match_args([has_entries(name='foo')]), match_kwargs({}))
     value = call({'name': 'baz'})
     s = StringDescription()
